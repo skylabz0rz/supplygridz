a4a366bbe40507cc83945f3b3feb0605464904b1
Adam Harden <adam@nationalmotorsports.com>
2025-05-11 17:37:43 -0500

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, Blueprint
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://admin:X8r9vPq2wLmA@db:5432/supplygridz'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://admin:adminpass@db:5432/supplygridz'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
