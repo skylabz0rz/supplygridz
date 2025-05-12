887dbca4bd0acb7c02a1da601e35f5b524ee4a2c
Adam Harden <adam@nationalmotorsports.com>
2025-05-11 21:57:54 -0500

    Auth0

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,77 @@
 from flask import Flask, jsonify, Blueprint
 from flask_sqlalchemy import SQLAlchemy
 
+from flask_cors import CORS
+from jose import jwt
+from flask import request, _request_ctx_stack
+import urllib.request
+import json
+
 app = Flask(__name__)
+CORS(app)
+
+AUTH0_DOMAIN = "dev-tzh46biettai7rin.us.auth0.com"
+API_AUDIENCE = "https://supplygridz.com/api"
+ALGORITHMS = ["RS256"]
+
+from functools import wraps
+from jose.exceptions import JWTError
+
+def get_token_auth_header():
+    auth = request.headers.get("Authorization", None)
+    if not auth:
+        raise Exception("Authorization header is missing")
+
+    parts = auth.split()
+
+    if parts[0].lower() != "bearer":
+        raise Exception("Authorization header must start with Bearer")
+    elif len(parts) == 1:
+        raise Exception("Token not found")
+    elif len(parts) > 2:
+        raise Exception("Authorization header must be Bearer token")
+
+    return parts[1]
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = get_token_auth_header()
+        jsonurl = urllib.request.urlopen(f"https://{AUTH0_DOMAIN}/.well-known/jwks.json")
+        jwks = json.loads(jsonurl.read())
+
+        unverified_header = jwt.get_unverified_header(token)
+        rsa_key = {}
+        for key in jwks["keys"]:
+            if key["kid"] == unverified_header["kid"]:
+                rsa_key = {
+                    "kty": key["kty"],
+                    "kid": key["kid"],
+                    "use": key["use"],
+                    "n": key["n"],
+                    "e": key["e"]
+                }
+
+        if not rsa_key:
+            raise Exception("Unable to find appropriate key")
+
+        try:
+            payload = jwt.decode(
+                token,
+                rsa_key,
+                algorithms=ALGORITHMS,
+                audience=API_AUDIENCE,
+                issuer=f"https://{AUTH0_DOMAIN}/"
+            )
+        except JWTError as e:
+            raise Exception("Token is invalid: " + str(e))
+
+        _request_ctx_stack.top.current_user = payload
+        return f(*args, **kwargs)
+
+    return decorated
+
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://admin:adminpass@db:5432/supplygridz'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -35,8 +105,10 @@ def players():
     return jsonify([{"id": p.id, "name": p.name} for p in Player.query.all()])
 
 @api.route("/vehicles")
+@requires_auth
 def vehicles():
     return jsonify([v.to_dict() for v in Vehicle.query.all()])
+
 
 app.register_blueprint(api)
 
