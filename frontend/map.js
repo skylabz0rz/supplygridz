63a5935ea023d6dd395342004c6946c46d0e6996
Adam Harden <adam@nationalmotorsports.com>
2025-05-12 21:52:26 -0500

    Update map.js

--- frontend/map.js
+++ frontend/map.js
@@ -1,7 +1,5 @@
-
-// Create and  expose the map globally
-window.map = L.map('map').setView([39.5, -98.35], 5); // center of US
-
+export const map = L.map('map').setView([39.5, -98.35], 5);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(window.map);
+  attribution: '&copy; OpenStreetMap contributors'
+}).addTo(map);
+
