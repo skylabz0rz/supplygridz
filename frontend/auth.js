eda492400bb8d290fd8b6ef14c29913cd69e0e41
skylabz0rz <adam@nationalmotorsports.com>
2025-05-13 08:22:25 -0500

    Update auth.js

--- frontend/auth.js
+++ frontend/auth.js
@@ -6,7 +6,7 @@ let auth0Client = null;
   try {
     auth0Client = await createAuth0Client({
       domain: "dev-tzh46biettai7rin.us.auth0.com",
-      client_id: "km3Gmbm6K9aeA3uFQh4Wlw3FSUjhZwr",
+      client_id: "km3gCmbm6K9aeA3uFQh4Wlw3FSUjhZwr",
       audience: "https://supplygridz.com/api",
       cacheLocation: 'localstorage'
     });
