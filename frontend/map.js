a7dcf0fd7a1169eeb67041e146b06d8403c397bc
Adam Harden <adam@nationalmotorsports.com>
2025-05-12 21:05:31 -0500

    Update map.js

--- frontend/map.js
+++ frontend/map.js
@@ -1,5 +1,5 @@
 
-let map = L.map('map').setView([39.5, -98.35], 4); // Center of continental US
+let map = L.map('map').setView([39.5, -98.35], 5);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
